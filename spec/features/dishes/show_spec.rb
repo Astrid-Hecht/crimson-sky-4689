ac68552b2ca9be5c915b196553c358c961885584
Astrid Hecht <silentaviary@gmail.com>
2022-09-20 15:46:22 -0600

    test: Write Dish Show feature tests

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish Show Page', type: :feature do
+  describe "As a visitor, When I visit a dish's show page" do
+    let!(:guy) {Chef.create!(name: 'Guy Pierry')}
+    let!(:bbb) {Dish.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
+    let!(:bread) {Ingredient.create!(name: 'French Bread', calories: 600)}
+    let!(:bacon) {Ingredient.create!(name: 'Bacon (Jamon)', calories: 950)}
+    before(:each) do
+      visit dish_path(:bbb)
+    end
+
+    it 'I see the dish’s name and description' do
+      within '#name' do
+        expect(page).to have_content(bbb.name)
+      end
+      within '#descript' do
+        expect(page).to have_content(bbb.description)
+      end
+    end
+
+    it 'And I see a list of ingredients for that dish' do
+      within '#ingredients' do
+        expect(page).to have_content(bread.name)
+        expect(page).to have_content(bacon.name)
+      end
+    end
+
+    it "And I see the chef's name" do
+      within '#chef' do
+        expect(page).to have_content(guy.name)
+      end
+    end
+  end
+end
