478a4dd66801050eb5e9272a7615abf31fc81d09
Astrid Hecht <silentaviary@gmail.com>
2022-09-20 16:06:55 -0600

    feat: Dish Show page & US 1 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Dish Show Page', type: :feature do
   describe "As a visitor, When I visit a dish's show page" do
     let!(:guy) {Chef.create!(name: 'Guy Pierry')}
-    let!(:bbb) {Dish.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
+    let!(:bbb) {guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
     let!(:bread) {Ingredient.create!(name: 'French Bread', calories: 600)}
     let!(:bacon) {Ingredient.create!(name: 'Bacon (Jamon)', calories: 950)}
+    let!(:bbb_recip1) {Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id )}
+    let!(:bbb_recip2) {Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id )}
+
     before(:each) do
-      visit dish_path(:bbb)
+      visit dish_path(bbb)
     end
 
     it 'I see the dish’s name and description' do
