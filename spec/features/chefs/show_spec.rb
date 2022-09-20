64d380b38bf3707e8246a16ca5322b0cf3bb38ec
Astrid Hecht <silentaviary@gmail.com>
2022-09-20 17:43:23 -0600

    test: extension tests"

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -3,11 +3,24 @@ require 'rails_helper'
 RSpec.describe 'Chef Show Page', type: :feature do
   describe "As a visitor, When I visit a Chef's show page" do
     let!(:guy) {Chef.create!(name: 'Guy Pierry')}
+
+    let!(:bt) {guy.dishes.create!(name: 'BL(!T)', description: 'delicious lettuce')}
     let!(:bbb) {guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
+    let!(:just_bacon) {guy.dishes.create!(name: 'Bacon', description: 'Huh... its just bacon')}
+    
     let!(:bread) {Ingredient.create!(name: 'French Bread', calories: 600)}
     let!(:bacon) {Ingredient.create!(name: 'Bacon (Jamon)', calories: 950)}
+    let!(:lettuce) { Ingredient.create!(name: 'Iceberg Lettuce', calories: 5) }
+
+    let!(:bt_recip1) {Recipie.create!(dish_id: bt.id, ingredient_id: bread.id )}
+    let!(:bt_recip2) {Recipie.create!(dish_id: bt.id, ingredient_id: bacon.id )}
+    let!(:bt_recip3) {Recipie.create!(dish_id: bt.id, ingredient_id: lettuce.id )}
+    
     let!(:bbb_recip1) {Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id )}
     let!(:bbb_recip2) {Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id )}
+    
+
+    let!(:just_bacon_recip1) {Recipie.create!(dish_id: just_bacon.id, ingredient_id: bacon.id )}
 
     before(:each) do
       visit chef_path(guy)
@@ -16,6 +29,13 @@ RSpec.describe 'Chef Show Page', type: :feature do
     it "I see the chef's name " do
       within '#name' do
         expect(page).to have_content('Name: ' + guy.name)
+      end
+    end
+
+    it 'I see the chefs three most freq used ingredients' do
+      within '#top_ingr' do
+        expect(bacon.name).to appear_before(bread.name)
+        expect(bread.name).to appear_before(lettuce.name)
       end
     end
 
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,25 +7,45 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it { should have_many :dishes }
   end
-
-  let!(:guy) { Chef.create!(name: 'Guy Pierry') }
-  let!(:bbb) { guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor') }
-  let!(:bread) { Ingredient.create!(name: 'French Bread', calories: 600) }
-  let!(:bacon) { Ingredient.create!(name: 'Bacon (Jamon)', calories: 950) }
+  
+  let!(:guy) {Chef.create!(name: 'Guy Pierry')}
+  
+  let!(:bt) {guy.dishes.create!(name: 'BL(!T)', description: 'delicious lettuce')}
+  let!(:bbb) {guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
+  let!(:just_bacon) {guy.dishes.create!(name: 'Bacon', description: 'Huh... its just bacon')}
+  
+  let!(:bread) {Ingredient.create!(name: 'French Bread', calories: 600)}
+  let!(:bacon) {Ingredient.create!(name: 'Bacon (Jamon)', calories: 950)}
   let!(:lettuce) { Ingredient.create!(name: 'Iceberg Lettuce', calories: 5) }
-  let!(:bbb_recip1) { Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id) }
-  let!(:bbb_recip2) { Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id) }
+  let!(:tomato) { Ingredient.create!(name: 'Tomato', calories: 5) }
+
+  let!(:bt_recip1) {Recipie.create!(dish_id: bt.id, ingredient_id: bread.id )}
+  let!(:bt_recip2) {Recipie.create!(dish_id: bt.id, ingredient_id: bacon.id )}
+  let!(:bt_recip3) {Recipie.create!(dish_id: bt.id, ingredient_id: lettuce.id )}
+  
+  let!(:bbb_recip1) {Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id )}
+  let!(:bbb_recip2) {Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id )}
+  
+
+  let!(:just_bacon_recip1) {Recipie.create!(dish_id: just_bacon.id, ingredient_id: bacon.id )}
 
   describe 'instance methods' do
     describe '#ingr_used' do
       it 'returns a list of used ingredients' do
-        expect(guy.ingr_used).to eq([bread, bacon])
+        expect(guy.ingr_used).to eq([bread, bacon, lettuce])
       end
 
       it 'doesnt return unused ones' do
-        expect(guy.ingr_used.include?(lettuce)).to be(false)
+        expect(guy.ingr_used.include?(tomato)).to be(false)
       end
 
     end
+
+    describe '#top_ingr' do
+      it 'returns list of used ingr by frequency' do
+        expect(guy.top_ingr).to eq([bacon, bread, letuce])
+      end
+    end
+
   end
 end
