5464b98ee9b059c1b3c20b406681895e07283abb
Astrid Hecht <silentaviary@gmail.com>
2022-09-20 16:28:15 -0600

    test: write chef show feature and chef model tests

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish Show Page', type: :feature do
+  describe "As a visitor, When I visit a dish's show page" do
+    let!(:guy) {Chef.create!(name: 'Guy Pierry')}
+    let!(:bbb) {guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor')}
+    let!(:bread) {Ingredient.create!(name: 'French Bread', calories: 600)}
+    let!(:bacon) {Ingredient.create!(name: 'Bacon (Jamon)', calories: 950)}
+    let!(:bbb_recip1) {Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id )}
+    let!(:bbb_recip2) {Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id )}
+
+    before(:each) do
+      visit chef_path(guy)
+    end
+
+    it "I see the chef's name " do
+      within '#name' do
+        expect(page).to have_content(bbb.name)
+      end
+    end
+
+    it 'And I see a link to view a list of all ingredients that this chef uses in their dishes' do
+      expect(page).to have_link("Ingredients Used")
+      # within '#ingr_arsenal' do
+      #   guy.used_ingredients.each do |ingredient|
+      #     expect(page).to have_content(ingredient.name)
+      #   end
+      # end
+    end
+
+    it "When I click on that link I'm taken to a chef's ingredient index page" do
+      click_link('Ingredients Used')
+      expect(current_path).to eq(chef_ingredients_path(guy))
+    end
+  end
+end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -2,9 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Chef, type: :model do
   describe "validations" do
-    it {should validate_presence_of :name}
+    it { should validate_presence_of :name }
   end
   describe "relationships" do
-    it {should have_many :dishes}
+    it { should have_many :dishes }
+  end
+
+  let!(:guy) { Chef.create!(name: 'Guy Pierry') }
+  let!(:bbb) { guy.dishes.create!(name: 'Bacon Baugette Bonanza', description: 'From Paris, the town of flavor') }
+  let!(:bread) { Ingredient.create!(name: 'French Bread', calories: 600) }
+  let!(:bacon) { Ingredient.create!(name: 'Bacon (Jamon)', calories: 950) }
+  let!(:lettuce) { Ingredient.create!(name: 'Iceberg Lettuce', calories: 5) }
+  let!(:bbb_recip1) { Recipie.create!(dish_id: bbb.id, ingredient_id: bread.id) }
+  let!(:bbb_recip2) { Recipie.create!(dish_id: bbb.id, ingredient_id: bacon.id) }
+
+  describe 'instance methods' do
+    describe '#ingr_used' do
+      it 'returns a list of used ingredients' do
+        expect(guy.ingr_used).to eq([bread, bacon])
+      end
+
+      it 'doesnt return unused ones' do
+        excpect(guy.ingr_used.include?(lettuce)).to be(false)
+      end
+
+    end
   end
 end
